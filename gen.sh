ef591567ab43a2271e3ab97aec91445daf55fddf
xrz <3545270592@qq.com>
2022-04-27 16:48:02 +0800

    20220427  Build From CI

--- gen.sh
+++ gen.sh
@@ -1,19 +1,19 @@
-rm ./lib/README.md
-cp ./lib/_sidebar.md ./lib/README.md
-rm -rf ./docs/lib
-cp -r ./lib ./docs/lib
+rm -rf './lib/README.md'
+cp './lib/_sidebar.md' './lib/README.md'
+rm -rf './docs/lib'
+cp -r './lib' './docs/lib'
 
-rm -rf ./docs/help
-cp -r ./help ./docs/help
+rm -rf './docs/help'
+cp -r './help' './docs/help'
 
-rm -rf ./docs/log
-cp -r ./log ./docs/log
+rm -rf './docs/log'
+cp -r './log' './docs/log'
 
-rm ./docs/README.md
-cp ./README.md ./docs/README.md
-rm ./docs/404.md
-cp ./404.md ./docs/404.md
+rm -rf './docs/README.md'
+cp './README.md' './docs/README.md'
+rm -rf './docs/404.md'
+cp './404.md' './docs/404.md'
 
 yarn docs:build
-rm -rf ./beta
-mv ./docs/.vuepress/dist ./beta
+rm -rf './beta'
+mv './docs/.vuepress/dist' './beta'
